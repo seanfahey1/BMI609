92616a1d8273869404ed36c2aa2e91ac3f849680
Sean <sfahey1745@sdsu.edu>
2024-03-17 14:08:41 -0700

    'midterm stuff'

--- Midterm1/Question3.py
+++
@@ -1,5 +0,0 @@
-"""
-Do Lab 3 (already uploaded to your Canvas page) until Exercise 2. Submit a RAxML-computed phylogeny for a set of
-aligned H3N2 genomes that you obtain at the end of the lab. Discuss these results - how do the genomes of H3N2 evolve?
-(30 points)
-"""
