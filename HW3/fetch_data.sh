d599ae64dc2e309f0bca313a3baa7851d9aa6493
Sean <sfahey1745@sdsu.edu>
2024-03-24 15:27:28 -0700

    'renamed main shell script'

--- HW3/fetch_data.sh
+++
@@ -1,4 +0,0 @@
-wget ftp://ftp.ebi.ac.uk/pub/training/Train_online/RNA-seq_exercise/2cells_1.fastq
-wget ftp://ftp.ebi.ac.uk/pub/training/Train_online/RNA-seq_exercise/2cells_2.fastq
-wget ftp://ftp.ebi.ac.uk/pub/training/Train_online/RNA-seq_exercise/6h_1.fastq
-wget ftp://ftp.ebi.ac.uk/pub/training/Train_online/RNA-seq_exercise/6h_2.fastq
