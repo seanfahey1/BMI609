e102cec97ae70494236fe4cf42b2a5ab2ad70c9f
Sean <sfahey1745@sdsu.edu>
2024-03-24 15:57:42 -0700

    'python cleanup'

--- HW3/merge_outputs.py
+++ HW3/merge_outputs.py
@@ -24,19 +24,29 @@ def extract_gene_id(row):
 
 
 def main():
+    # open files as data frames
     with open("DESeq2_results.csv", "r") as f:
         df = pd.read_csv(f, header=0)
+    df.index.name = "GeneID"
 
     with open("danio_genome/Danio_rerio.Zv9.66.gtf", "r") as gtf:
         gtf = pd.read_csv(gtf, header=None, sep="\t", low_memory=False)
 
+    # extract desired data into clean columns
     gtf["gene_name"] = gtf.apply(extract_gene_name, axis=1)
     gtf["gene_id"] = gtf.apply(extract_gene_id, axis=1)
+
+    # hold on to only needed columns
     gtf.set_index("gene_id", inplace=True)
     gtf = gtf["gene_name"]
 
+    # convert to dict
     gtf_dict = gtf.to_dict()
+
+    # map dict into new col
     df["gene_name"] = df.index.map(gtf_dict)
+
+    # sort and store
     df = df.sort_values(by="padj", ascending=True)
 
     with open("DESeq2_results_gene_name.csv", "w") as out:
