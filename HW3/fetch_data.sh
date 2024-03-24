2238292d802aefcb36eba6eb1a45fc3a56ae1cbd
Sean <sfahey1745@sdsu.edu>
2024-03-23 22:31:58 -0700

    'dir structure'

---
+++ HW3/fetch_data.sh
@@ -0,0 +1,4 @@
+wget ftp://ftp.ebi.ac.uk/pub/training/Train_online/RNA-seq_exercise/2cells_1.fastq
+wget ftp://ftp.ebi.ac.uk/pub/training/Train_online/RNA-seq_exercise/2cells_2.fastq
+wget ftp://ftp.ebi.ac.uk/pub/training/Train_online/RNA-seq_exercise/6h_1.fastq
+wget ftp://ftp.ebi.ac.uk/pub/training/Train_online/RNA-seq_exercise/6h_2.fastq
