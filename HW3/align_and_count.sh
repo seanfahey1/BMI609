ffbc87126ad1664ad5129e27b0a64820b70a0851
Sean <sfahey1745@sdsu.edu>
2024-03-24 02:23:40 -0700

    'python script to join outputs'

--- HW3/align_and_count.sh
+++ HW3/align_and_count.sh
@@ -9,6 +9,9 @@ done
 # install deps. for deseq2
 sudo apt-get install -y libxml2-dev libcurl4-openssl-dev
 
+# install python deps
+pip install pandas HTSeq
+
 # install and source STAR
 cd /home/exouser/tools
 wget https://github.com/alexdobin/STAR/archive/2.7.11b.tar.gz && tar -xzf 2.7.11b.tar.gz
@@ -27,7 +30,6 @@ STAR --genomeDir danio_genome/STAR --readFilesIn 2cells_1.fastq 2cells_2.fastq -
 STAR --genomeDir danio_genome/STAR --readFilesIn 6h_1.fastq 6h_2.fastq --runThreadN 12 --outFileNamePrefix 6hour_aligned
 
 # htseq counts table
-pip install HTSeq
 python -m HTSeq.scripts.count 2cell_alignedAligned.out.sam danio_genome/Danio_rerio.Zv9.66.gtf > htseq_count_2_cell.txt
 python -m HTSeq.scripts.count 6hour_alignedAligned.out.sam danio_genome/Danio_rerio.Zv9.66.gtf > htseq_count_6_hour.txt
 
--- HW3/merge_outputs.py
+++ HW3/merge_outputs.py
@@ -1,7 +1,44 @@
 #!/usr/bin/env python3
 import pandas as pd
+import re
+import sys
 
 
-with open("DESeq2_results.csv", "r") as f:
-    df = pd.read_csv(f, header=0)
-df
+regex = r'gene_id "(.*)"; transcript_id ".*"; exon_number "\d+"; gene_name "(.*)"; gene_biotype'
+
+
+def extract_gene_name(row):
+    match = re.search(regex, row[8])
+    if match is not None:
+        return match.group(2)
+    else:
+        return None
+
+
+def extract_gene_id(row):
+    match = re.search(regex, row[8])
+    if match is not None:
+        return match.group(1)
+    else:
+        return None
+
+
+def main():
+    with open("DESeq2_results.csv", "r") as f:
+        df = pd.read_csv(f, header=0)
+
+    with open("danio_genome/Danio_rerio.Zv9.66.gtf", "r") as gtf:
+        gtf = pd.read_csv(gtf, header=None, sep="\t", low_memory=False)
+
+    gtf["gene_name"] = gtf.apply(extract_gene_name, axis=1)
+    gtf["gene_id"] = gtf.apply(extract_gene_id, axis=1)
+    gtf.set_index("gene_id", inplace=True)
+    gtf = gtf["gene_name"]
+
+    df = df.join(gtf)
+    with open("DESeq2_results_gene_name.csv", "w") as out:
+        out.write(df.to_csv(sep=",", header=True, index=True))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
