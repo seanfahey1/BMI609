4a6b8d7a0e82cf2ec8dfe4dab5943d4d5a3985d1
Sean <sfahey1745@sdsu.edu>
2024-03-24 15:52:33 -0700

    'readme and adding lower pval cutoff for DESeq FDR correction'

--- HW3/deseq2.R
+++ HW3/deseq2.R
@@ -1,46 +1,46 @@
 if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
+  install.packages("BiocManager")
 
 BiocManager::install("DESeq2")
 
 library("DESeq2")
 directory <- "/home/exouser/workspace/BMI609/HW3/"
 
-sampleFiles <- grep(
-    "htseq_count_.*txt",
-    list.files(directory),
-    value=TRUE
-    )
+sample_files <- grep(
+  "htseq_count_.*txt",
+  list.files(directory),
+  value = TRUE
+)
 
-sampleCondition <- sub("htseq_count_(.*).txt","\\1",sampleFiles)
+sample_condition <- sub("htseq_count_(.*).txt", "\\1", sample_files)
 
-sampleTable <- data.frame(
-    sampleName = sampleFiles,
-    fileName = sampleFiles,
-    condition = sampleCondition
-    )
+sample_table <- data.frame(
+  sampleName = sample_files,
+  fileName = sample_files,
+  condition = sample_condition
+)
 
-sampleTable$condition <- factor(sampleTable$condition)
+sample_table$condition <- factor(sample_table$condition)
 
-ddsHTSeq <- DESeqDataSetFromHTSeqCount(
-    sampleTable = sampleTable,
-    directory = directory,
-    design= ~ 1
-    )
+dds_ht_seq <- DESeqDataSetFromHTSeqCount(
+  sampleTable = sample_table,
+  directory = directory,
+  design = ~ 1
+)
 
-dds <- DESeq(ddsHTSeq)
-res <- results(dds)
-res_clean = res[!is.na(res$padj),]
-res_clean = subset(res_clean, padj < 0.05)
+dds <- DESeq(dds_ht_seq)
+res <- results(dds, alpha = 0.05)
+res_clean <- res[!is.na(res$padj), ]
+res_clean <- subset(res_clean, padj < 0.05)
 
 write.table(
-    res_clean,
-    file = "DESeq2_results.csv",
-    append = FALSE,
-    quote = TRUE,
-    sep = ",",
-    eol = "\n",
-    na = "NA",
-    row.names = TRUE,
-    col.names = TRUE,
-    )
+  res_clean,
+  file = "DESeq2_results.csv",
+  append = FALSE,
+  quote = TRUE,
+  sep = ",",
+  eol = "\n",
+  na = "NA",
+  row.names = TRUE,
+  col.names = TRUE,
+)
