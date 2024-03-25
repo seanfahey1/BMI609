969ddebc7067b179ab1304c950e70f9a17fef33b
Sean <sfahey1745@sdsu.edu>
2024-03-24 17:28:08 -0700

    'first commit'

---
+++ Midterm2/Question1.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+# For this exam, you will use the corresponding paired end reads to run through the entire MOTHUR/QIIME2/DADA2/KRAKEN2 pipeline (just pick one). IMPORTANT: No Galaxy use! You will do all analyses in the command line interface of the associated tool, collate all code you used and submit the code with your exam.
+
+# Tutorials, installation instructions for each tool are here (you only need to run your data through one of those pipelines):
+# DADA2 - https://benjjneb.github.io/dada2
+# MOTHUR - https://mothur.org/wiki/miseq_sop
+# QIIME2 - https://docs.qiime2.org/2024.2/interfaces/q2cli
+# KRAKEN2 - https://github.com/DerrickWood/kraken2/wiki/Manual
+
+# Thereon, each one of you will use the FASTQ files I've assigned you (see below), and analyze it. Then, submit all plots, result files, and your code used in a Zipped folder. You'll find all FASTQ files under Files->Assignment Files->Midterm 2
+# zr13074_4V3V4_R1.fastq.gz and zr13074_4V3V4_R1.fastq.gz
